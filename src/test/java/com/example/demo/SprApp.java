9b48ce1f73f34f585709bfca7488ccf1c6726ea4
wrzhxy <wrzhxy@qq.com>
2017-10-20 17:37:00 +0800

    a

---
+++ src/test/java/com/example/demo/SprApp.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class SprApp {
+	
+	@RequestMapping("/")
+	String home() {
+		return "你好，世界!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SprApp.class, args);
+	}
+
+}
